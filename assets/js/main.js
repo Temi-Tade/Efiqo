e6b226e2b3d73bf5535fd04bef417ff465989c2d
Temi-Tade <akintadetemi19@gmail.com>
2025-05-16 11:14:42 +0100

    update beta users list

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,8 +85,13 @@ const BETA_USERS = [
         email: "paulabodunrin03@gmail.com"
     },
     {
-     firstName: "Daniel",
-     lastName: "Akinade",
-     email: "danielscholaressay04@gmail.com"
- }
+        firstName: "Daniel",
+        lastName: "Akinade",
+        email: "danielscholaressay04@gmail.com"
+    },
+    {
+        firstName: "Christiana",
+        lastName: "Ilesanmi",
+        email: "ilesanmichristiana29@gmail.com"
+    }
 ];
